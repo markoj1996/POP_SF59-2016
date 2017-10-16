198e87befe0cacaf3835ca6c901f0008e2683105
student <student@ftn.ns>
2017-10-16 12:32:14 +0200

    init

---
+++ POP_SF59-2016/Tests/A.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_SF59_2016
+{
+    class A
+    {
+        private string naziv;
+        private string ime;
+
+        public string GetNaziv()
+        {
+            return this.naziv;
+        }
+
+        public void SetNaziv(string naziv)
+        {
+            this.naziv = naziv;
+        }
+
+        public string Ime
+        {
+            get
+            {
+                return this.ime;
+            }
+            set
+            {
+                this.ime = value;
+            }
+        }
+    }
+}
